84ba08d776ae07909a705155cf6764a969c74bc0
rvdm2101 <rvdmolen2101@gmail.com>
2024-05-06 11:19:11 +0200

    Fixed ImageSelectModal styling

--- resources/js/Components/Modal/ImageSelectModal.tsx
+++ resources/js/Components/Modal/ImageSelectModal.tsx
@@ -10,9 +10,9 @@ interface ImageSelectModalProps {
 
 const ImageSelectModal = ({ show, onClose, images, selectImage }: ImageSelectModalProps) => (
     <Modal show={show} onClose={onClose} maxWidth="7xl">
-        <section className="p-6 flex flex-row gap-4">
+        <section className="p-6 grid grid-cols-6 gap-4">
             {images.length ? images.map(image => (
-                <button key={image.id} className="basis-1/6 bg-white shadow-lg hover:drop-shadow-md active:drop-shadow-lg rounded-2xl border border-gray-500 active:border-indigo-500 overflow-hidden" type="button" onClick={() => selectImage(image.id)}>
+                <button key={image.id} className="bg-white shadow-lg hover:drop-shadow-md active:drop-shadow-lg rounded-2xl border border-gray-500 active:border-indigo-500 overflow-hidden" type="button" onClick={() => selectImage(image.id)}>
                     <img className="h-40 p-2 mx-auto" src={image.path} />
                     <div className="w-full p-2 bg-gray-100">
                         <h3>{image.filename}</h3>
